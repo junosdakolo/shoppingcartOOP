c194c58556366ed772193ba0bd5df92e57e559d4
junosdakolo <junosdakolo@gmail.com>
2025-01-28 20:22:42 +0100

    6th  commit

--- js/class.js
+++ js/class.js
@@ -23,6 +23,7 @@ class ShoppingCartItem{
 class ShoppingCart{
     constructor(items=[]){
         this.items = items;
+        this.cartTotal = 0;
     }
     // Get the total of items inside the cart
     totalItems(){
@@ -64,10 +65,72 @@ class ShoppingCart{
     }
     // Display cart items
     displayCartItems(){
-        const cartContainer = document.getElementById('')
-
-
+        let total = 0;
+        this.items.forEach(item=>{
+            total += item.getTotalPrice();
+        });
+        this.cartTotal = total;
+        document.querySelector('#total').innerText = `Total: $${total}`;
     }
+
+
+
+
+
+    displayProducts() {
+        if (!this.products.length) {
+          return;
+        }
+    
+        const wrapper = document.querySelector('.products-grid');
+        if (!wrapper) {
+          return;
+        }
+    
+        const existingElements = document.querySelectorAll('.product-outer');
+        existingElements.forEach(el => el.remove());
+    
+        this.products.forEach(product => {
+          const itemIsInCart = this.itemIsInCart(product.id);
+    
+          const productCard = document.createElement('section');
+          productCard.setAttribute('id', product.id);
+          productCard.setAttribute('class', 'product-outer');
+    
+          productCard.innerHTML = `
+            <div class="product">
+              <img class="w-full h-[200px] object-contain" src="https://res.cloudinary.com/dk07kf3yl/image/upload/c_thumb,w_200,g_face/v1733673243/temp/item-placeholder_pr2sx5.webp" alt="">
+              <div class="bg-gray-100 p-3">
+                <div class="flex justify-between h-[50px]">
+                  <div>${product.name}</div>
+                  <div>$${product.price}</div>
+                </div>
+                <div class="mt-3">
+                  <button class="bg-gray-700 text-white text-xs p-2 rounded">
+                    ${itemIsInCart ? 'Remove From Cart' : 'Add To Cart'}
+                  </button>
+                </div>
+              </div>
+            </div>
+          `;
+    
+          productCard.querySelector('button')?.addEventListener('click', (event) => {
+            const button = event.currentTarget;
+            const inCart = this.itemIsInCart(product.id);
+            inCart ? this.removeCartItem(product.id) : this.addCartItem(product);
+            button.textContent = inCart ? 'Add To Cart' : 'Remove From Cart';
+          })
+    
+          wrapper.appendChild(productCard);
+        })
+      }
+    
+      clear() {
+        this.items = [];
+        this.displayProducts();
+        this.displayCartTotal();
+      }
+    
 }
 
 
